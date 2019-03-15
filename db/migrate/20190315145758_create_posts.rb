3b49eea18cea6bbf1a9eeb18a3089155087db506
Nicole D <nnardella@gmail.com>
2019-03-15 14:58:25 +0000

    Automatically backed up by Learn

--- db/migrate/20190315145758_create_posts.rb
+++ db/migrate/20190315145758_create_posts.rb
@@ -2,9 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :string
-      t.string :description
-      t.string :text
+      t.text :description
 
       t.timestamps null: false
     end
